3e0d083fbf35d225cbe938546a4398e4958b86c9
yonggaljjw <jjw627@naver.com>
2025-02-06 14:46:36 +0900

    react docker 완

--- project/frontend/src/components/Navbar.js
+++ project/frontend/src/components/Navbar.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../App.css"; // 스타일 적용
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">AI 면접 도우미</h1>
-        <ul className="flex space-x-4">
-          <li><Link to="/" className="hover:underline">홈</Link></li>
-          <li><Link to="/interview" className="hover:underline">면접 시작</Link></li>
-          <li><Link to="/results" className="hover:underline">면접 결과</Link></li>
-          <li><Link to="/settings" className="hover:underline">설정</Link></li>
-        </ul>
-      </div>
+    <nav className="navbar">
+      {/* ✅ I.M.S 클릭 시 홈으로 이동 */}
+      <h1>
+        <Link to="/" className="home-link">I.M.S</Link>
+      </h1>
+      <ul>
+        <li><Link to="/">홈</Link></li>
+        <li><Link to="/interview">면접 시작</Link></li>
+        <li><Link to="/results">면접 결과</Link></li>
+        <li><Link to="/settings">설정</Link></li>
+      </ul>
     </nav>
   );
 };
