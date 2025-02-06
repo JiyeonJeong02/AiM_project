ece35d61f54022699c8be492e1a6992ee825d163
yonggaljjw <jjw627@naver.com>
2025-02-06 14:46:36 +0900

    react docker 완

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -5,12 +5,13 @@ import Home from "./pages/Home";
 import Interview from "./pages/Interview";
 import Results from "./pages/Results";
 import Settings from "./pages/Settings";
+import "./App.css"; // 스타일 적용
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <div className="container mx-auto mt-8">
+      <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/interview" element={<Interview />} />
