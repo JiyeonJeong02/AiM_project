07adfe546702d5fb77e742b1a94d89c255a1b51e
yonggaljjw <jjw627@naver.com>
2025-02-07 12:45:11 +0900

    오류 수정

--- project/frontend/src/components/SpeechRecognition.js
+++ project/frontend/src/components/SpeechRecognition.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const SpeechRecognitionComponent = ({ onResult }) => {
   const [isListening, setIsListening] = useState(false);
